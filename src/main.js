8871b0d7d3387b1b4ddf41d4b19cc4beb9935f6f
Jose Mayol <5716648+jei-mayol@users.noreply.github.com>
2020-05-20 02:24:22 +0800

    Handle authentication state changes in main.js
    This will handle things like page reload and login. This makes sure that
    Firebase initializes before loading the app.
    
    Note: made no-unused-vars lint rule a warning for now as the tutorial
    adds a 'user' var but it's not used at this point. Might want to revert
    this back to error always.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+const fb = require('./firebaseConfig.js')
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+// Handle page reloads
+let app
+fb.auth.onAuthStateChanged(user => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
