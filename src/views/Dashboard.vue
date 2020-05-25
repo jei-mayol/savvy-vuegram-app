5417754f48f006d477c8beaaaa087f9c8bdc1172
Jose Mayol <5716648+jei-mayol@users.noreply.github.com>
2020-05-25 21:26:58 +0800

    Dashboard: Implement likes.
    A user should only be able to "like" a post once. Likes are saved in a
    separate collection where we associate the post id with the user id.
    
    If a user has already "liked" a post, no-op. Otherwise, create the
    "like" record and update the post's total likes.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -35,8 +35,8 @@
             <span>{{ post.createdOn | formatDate }}</span>
             <p>{{ post.content | trimLength }}</p>
             <ul>
-              <li><a @click="openCommentModal(post)">Comments {{ posts.comments }}</a></li>
-              <li><a>Likes {{ posts.likes }}</a></li>
+              <li><a @click="openCommentModal(post)">Comments {{ post.comments }}</a></li>
+              <li><a @click="likePost(post.id, post.likes)">Likes {{ post.likes }}</a></li>
               <li><a>View full post</a></li>
             </ul>
           </div>
@@ -151,6 +151,32 @@ export default {
       this.comment.userId = ''
       this.comment.content = ''
       this.showCommentModal = false
+    },
+    likePost(postId, postLikes) {
+      let docId = `${this.currentUser.uid}_${postId}`
+
+      firebase.likesCollection
+        .doc(docId)
+        .get()
+        .then(doc => {
+
+          if (doc.exists) {
+            return
+          }
+
+          firebase.likesCollection.doc(docId).set({
+            postId: postId,
+            userId: this.currentUser.uid
+          })
+          .then(() => {
+            firebase.postsCollection.doc(postId).update({
+              likes: postLikes + 1
+            })
+          })
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
   },
   filters: {
